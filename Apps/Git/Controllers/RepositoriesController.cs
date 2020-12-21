f036c6dc44f87300ad59c168b9dd90928ff8e03e
andreivorona <andreivorona@abv.bg>
2020-12-21 18:38:37 +0200

    Login Register and LogOut works.

---
+++ Apps/Git/Controllers/RepositoriesController.cs
@@ -0,0 +1,13 @@
+﻿namespace Git.Controllers
+{
+    using SUS.HTTP;
+    using SUS.MvcFramework;
+
+    public class RepositoriesController : Controller
+    {
+        public HttpResponse All()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Apps/Git/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿namespace Git.Controllers
+{
+    using Git.Services;
+    using Git.ViewModels.Users;
+    using SUS.HTTP;
+    using SUS.MvcFramework;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UsersController : Controller
+    {
+        private readonly IUsersService usersService;
+
+        public UsersController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        public HttpResponse Login()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Login(LoginInputModel input)
+        {
+            var userId = usersService.GetUserId(input.Username, input.Password);
+
+            if (userId == null)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            this.SignIn(userId);
+
+            return this.Redirect("/Repositories/All");
+        }
+
+        public HttpResponse Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Register(RegisterInputModel input)
+        {
+            if (string.IsNullOrEmpty(input.Username)
+                || input.Username.Length < 5
+                || input.Username.Length > 20)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (string.IsNullOrEmpty(input.Email)
+                || !new EmailAddressAttribute().IsValid(input.Email))
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (string.IsNullOrEmpty(input.Password)
+                || input.Password.Length < 6
+                || input.Password.Length > 20)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (input.Password != input.ConfirmPassword)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            this.usersService.CreateUser(input.Username, input.Email, input.Password);
+
+            return Redirect("/Users/Login");
+        }
+
+        public HttpResponse Logout()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/");
+            }
+
+            this.SignOut();
+            return this.Redirect("/");
+        }
+    }
+}
